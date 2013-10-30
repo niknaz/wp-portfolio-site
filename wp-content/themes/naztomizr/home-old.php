3189600aed360e82a336bfce0c6f212d28ff968d
niknaz <nyniknaz@gmail.com>
2013-10-29 20:26:36 -0400

    Attempt using a rendition of home.php from this page: http://wordpress.org/support/topic/how-may-display-a-custom-list-of-posts-on-a-page. Is not working so I'm abandoning maybe…

---
+++ wp-content/themes/naztomizr/home-old.php
@@ -0,0 +1,86 @@
+<?php
+/*
+Template Name: Home Page 
+*/
+?>
+<?php do_action( '__before_main_wrapper' ); ##hooks the header with get_header ?>
+<?php tc__f('rec' , __FILE__ , __FUNCTION__ ); ?>
+<div id="main-wrapper" class="container">
+    <?php /* do_action( '__before_main_container' ); ##hooks the featured page (priority 10) and breadcrumb (priority 20)...and whatever you need! naz: don't need this hook (found in class-cntent-featured_pages.php) */?>
+    <div class="container" role="main">
+        <div class="row">
+            <?php do_action( '__sidebar' , 'left' ); ?>
+                <div class="<?php echo tc__f( '__screen_layout' , tc__f ( '__ID' ) , 'class' ) ?> article-container">
+                    
+                    <?php do_action ('__before_loop');##hooks the header of the list of post : archive, search... ?>
+                        
+                        <?php
+                            global $wp_query;
+                            ##do we have posts? If not are we in the no search result case?
+                            if ( have_posts() || (is_search() && 0 == $wp_query -> post_count) ) : ?>
+                                <?php if ( is_search() && 0 == $wp_query -> post_count ) : ##no search results case ?>
+                                    <article <?php tc__f('__article_selectors') ?>>
+                                        <?php do_action( '__loop' ); ?>
+                                    </article>
+                                <?php endif; ?>
+
+                                <?php while ( have_posts() ) : ##all other cases for single and lists: post, custom post type, page, archives, search, 404 ?>
+                                	
+                                	<?php /* let's add our isotope */ ?>
+                                    <section id="primary" class="full-width isotope-primary">
+	                                	<header class="isotope-header">
+											<nav id="isotope-nav" class="secondary-nav span10 offset2" data-option-key="filter">
+											<?php  
+												/* let's get the categories here 
+												 * thanks to here: 
+												 * http://www.paulund.co.uk/display-categories-of-a-custom-post-type */
+												 
+											$customPostTaxonomies = get_object_taxonomies('naztomizr_portfolio');
+							
+												if(count($customPostTaxonomies) > 0)
+												{
+												     foreach($customPostTaxonomies as $tax)
+												     {
+													     $args = array(
+												         	  'orderby' => 'name',
+													          'show_count' => 0,
+												        	  'pad_counts' => 0,
+													          'hierarchical' => 1,
+												        	  'taxonomy' => $tax,
+												        	  'title_li' => ''
+												        	);
+												
+													     wp_list_categories( $args );
+												     }
+												} ?>
+											</nav><!-- .secondary-nav -->
+										</header><!-- .isotope-header -->
+
+                                    <?php the_post(); ?>
+                                    <article <?php tc__f('__article_selectors') ?>>
+                                        <?php
+                                        do_action( '__loop' );
+                                        ##we don't want to display more than one post if 404!
+                                        if ( is_404() )
+                                            break;
+                                        ?>
+                                    </article>
+                                    
+                                    </section> <!-- .isotope-primary -->
+                                <?php endwhile; ?>
+
+                            <?php endif; ##end if have posts ?>
+
+                    <?php do_action ('__after_loop');##hooks the comments and the posts navigation with priorities 10 and 20 ?>
+
+                </div><!--.article-container -->
+            <?php do_action( '__sidebar' , 'right' ); ?>
+        </div><!--.row -->
+    </div><!-- .container role: main -->
+    <?php do_action( '__after_main_container' ); ?>
+</div><!--#main-wrapper"-->
+<?php do_action( '__after_main_wrapper' );##hooks the footer with get_get_footer ?>
+
+
+*************
+
--- wp-content/themes/naztomizr/home.php
+++ wp-content/themes/naztomizr/home.php
@@ -1,86 +1,88 @@
 <?php
 /*
-Template Name: Home Page 
-*/
+ * Template Name: Home Page
+ * @package Customizr
+ * @since Customizr 3.0.12
+ */
 ?>
-<?php do_action( '__before_main_wrapper' ); ##hooks the header with get_header ?>
+
+<?php do_action( '__before_main_wrapper' ); ##hook of the header with get_header ?>
 <?php tc__f('rec' , __FILE__ , __FUNCTION__ ); ?>
 <div id="main-wrapper" class="container">
-    <?php /* do_action( '__before_main_container' ); ##hooks the featured page (priority 10) and breadcrumb (priority 20)...and whatever you need! naz: don't need this hook (found in class-cntent-featured_pages.php) */?>
+
+    <?php do_action( '__before_main_container' ); ##hook of the featured page (priority 10) and breadcrumb (priority 20)...and whatever you need! ?>
+
     <div class="container" role="main">
         <div class="row">
-            <?php do_action( '__sidebar' , 'left' ); ?>
+
+            <?php do_action( '__before_article_container'); ##hook of left sidebar?>
+
+                <?php query_posts('post_type=post&post_status=publish&posts_per_page=10&paged='. get_query_var('paged')); ?>
+
                 <div class="<?php echo tc__f( '__screen_layout' , tc__f ( '__ID' ) , 'class' ) ?> article-container">
-                    
+
                     <?php do_action ('__before_loop');##hooks the header of the list of post : archive, search... ?>
-                        
-                        <?php
-                            global $wp_query;
-                            ##do we have posts? If not are we in the no search result case?
-                            if ( have_posts() || (is_search() && 0 == $wp_query -> post_count) ) : ?>
-                                <?php if ( is_search() && 0 == $wp_query -> post_count ) : ##no search results case ?>
-                                    <article <?php tc__f('__article_selectors') ?>>
-                                        <?php do_action( '__loop' ); ?>
-                                    </article>
-                                <?php endif; ?>
 
-                                <?php while ( have_posts() ) : ##all other cases for single and lists: post, custom post type, page, archives, search, 404 ?>
-                                	
-                                	<?php /* let's add our isotope */ ?>
-                                    <section id="primary" class="full-width isotope-primary">
-	                                	<header class="isotope-header">
-											<nav id="isotope-nav" class="secondary-nav span10 offset2" data-option-key="filter">
-											<?php  
-												/* let's get the categories here 
-												 * thanks to here: 
-												 * http://www.paulund.co.uk/display-categories-of-a-custom-post-type */
-												 
-											$customPostTaxonomies = get_object_taxonomies('naztomizr_portfolio');
-							
-												if(count($customPostTaxonomies) > 0)
-												{
-												     foreach($customPostTaxonomies as $tax)
-												     {
-													     $args = array(
-												         	  'orderby' => 'name',
-													          'show_count' => 0,
-												        	  'pad_counts' => 0,
-													          'hierarchical' => 1,
-												        	  'taxonomy' => $tax,
-												        	  'title_li' => ''
-												        	);
-												
-													     wp_list_categories( $args );
-												     }
-												} ?>
-											</nav><!-- .secondary-nav -->
-										</header><!-- .isotope-header -->
+                        <?php if ( tc__f('__is_no_results') || is_404() ) : ##no search results or 404 cases ?>
+                            <article <?php tc__f('__article_selectors') ?>>
+                                <?php do_action( '__loop' ); ?>
+                            </article>
+                        <?php endif; ?>
 
-                                    <?php the_post(); ?>
-                                    <article <?php tc__f('__article_selectors') ?>>
-                                        <?php
+                        <?php if ( have_posts() && !is_404() ) : ?>
+                            <?php while ( have_posts() ) : ##all other cases for single and lists: post, custom post type, page, archives, search, 404 ?>
+                            	<?php /* CATEGORIES FOR ISOTOPE */?>
+                    			<header class="isotope-header">
+									<nav id="isotope-nav" class="secondary-nav span10 offset2" data-option-key="filter">
+									<?php  
+										/* let's get the categories here 
+										 * thanks to here: 
+										 * http://www.paulund.co.uk/display-categories-of-a-custom-post-type */
+										 
+									$customPostTaxonomies = get_object_taxonomies('naztomizr_portfolio');
+					
+										if(count($customPostTaxonomies) > 0)
+										{
+										     foreach($customPostTaxonomies as $tax)
+										     {
+											     $args = array(
+										         	  'orderby' => 'name',
+											          'show_count' => 0,
+										        	  'pad_counts' => 0,
+											          'hierarchical' => 1,
+										        	  'taxonomy' => $tax,
+										        	  'title_li' => ''
+										        	);
+										
+											     wp_list_categories( $args );
+										     }
+										} ?>
+									</nav><!-- .secondary-nav -->
+								</header><!-- .isotope-header -->
+
+                                <?php the_post(); ?>
+                                <article <?php tc__f('__article_selectors') ?>>
+                                    <?php
                                         do_action( '__loop' );
-                                        ##we don't want to display more than one post if 404!
-                                        if ( is_404() )
-                                            break;
-                                        ?>
-                                    </article>
-                                    
-                                    </section> <!-- .isotope-primary -->
-                                <?php endwhile; ?>
+                                    ?>
+                                </article>
+                            <?php endwhile; ?>
 
-                            <?php endif; ##end if have posts ?>
+                        <?php endif; ##end if have posts ?>
 
-                    <?php do_action ('__after_loop');##hooks the comments and the posts navigation with priorities 10 and 20 ?>
+                    <?php do_action ('__after_loop');##hook of the comments and the posts navigation with priorities 10 and 20 ?>
 
                 </div><!--.article-container -->
-            <?php do_action( '__sidebar' , 'right' ); ?>
+
+        	<?php wp_reset_query(); ?>
+
+        	<?php do_action( '__after_article_container'); ##hook of left sidebar?>
+
         </div><!--.row -->
     </div><!-- .container role: main -->
+
     <?php do_action( '__after_main_container' ); ?>
+
 </div><!--#main-wrapper"-->
-<?php do_action( '__after_main_wrapper' );##hooks the footer with get_get_footer ?>
 
-
-*************
-
+<?php do_action( '__after_main_wrapper' );##hook of the footer with get_get_footer ?>
